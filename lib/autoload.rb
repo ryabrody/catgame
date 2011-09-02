89036fa20071177c3de5b99229081a91b593640b
ryabrody <franca.rast@screenconcept.ch>
2011-09-02 15:45:50 +0200

    edit .gitignore

--- lib/autoload.rb
+++ lib/autoload.rb
@@ -1,20 +1,20 @@
 module Helper
-  autoload :MarkupHelper, '../lib/helper/markup_helper'
-  autoload :DeathHelper, '../lib/helper/death_helper'
-  autoload :Assets, '../lib/helper/assets.rb'
+  autoload :MarkupHelper, $APP_ROOT + 'lib/helper/markup_helper'
+  autoload :DeathHelper, $APP_ROOT + 'lib/helper/death_helper'
+  autoload :Assets, $APP_ROOT + 'lib/helper/assets.rb'
 end
 
 module Levels
-  autoload :FirstRoom, '../lib/levels/first_room'
-  autoload :FeedingRoom, '../lib/levels/feeding_room'
-  autoload :WashingRoom, '../lib/levels/washing_room'
-  autoload :TrainingRoom, '../lib/levels/training_room'
-  autoload :StylingRoom, '../lib/levels/styling_room'
-  autoload :GuestRoom, '../lib/levels/guest_room'
-  autoload :SleepingRoom, '../lib/levels/sleeping_room'
-  autoload :HappyEnd, '../lib/levels/happy_end'
-  autoload :Assets, '../lib/levels/assets'
-  autoload :Death, '../lib/levels/death' 
+  autoload :FirstRoom, $APP_ROOT + 'lib/levels/first_room'
+  autoload :FeedingRoom, $APP_ROOT + 'lib/levels/feeding_room'
+  autoload :WashingRoom, $APP_ROOT + 'lib/levels/washing_room'
+  autoload :TrainingRoom, $APP_ROOT + 'lib/levels/training_room'
+  autoload :StylingRoom, $APP_ROOT + 'lib/levels/styling_room'
+  autoload :GuestRoom, $APP_ROOT + 'lib/levels/guest_room'
+  autoload :SleepingRoom, $APP_ROOT + 'lib/levels/sleeping_room'
+  autoload :HappyEnd, $APP_ROOT + 'lib/levels/happy_end'
+  autoload :Assets, $APP_ROOT + 'lib/levels/assets'
+  autoload :Death, $APP_ROOT + 'lib/levels/death' 
 end
 
-autoload :Catgame, '../lib/catgame.rb'
+autoload :Catgame, $APP_ROOT + 'lib/catgame.rb'
